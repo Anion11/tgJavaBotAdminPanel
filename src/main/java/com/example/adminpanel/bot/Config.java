0377beb5a7b87ffd579992540397eee4f5a93371
Nikita Klyukoit <klukoyt@dterra.ru>
2024-06-30 15:33:57 +0300

    add config_test

---
+++ src/main/java/com/example/adminpanel/bot/Config.java
@@ -0,0 +1,13 @@
+package com.example.adminpanel.bot;
+
+public class Config {
+    private String bot_username = "BOT_USERNAME";
+    private String bot_token = "BOT_TOKEN";
+
+    public String getBotToken() {
+        return bot_token;
+    }
+    public String getBotUsername() {
+        return bot_username;
+    }
+}
